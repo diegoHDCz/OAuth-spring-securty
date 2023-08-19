d325ec4c09bf976466b7bbfa6b00bd3f05192728
Diego Czajka <diego.czajka@gmail.com>
2023-08-19 11:19:02 -0300

    :sparkles: feat Add change password

--- security-client/src/main/java/com/diegoczajka/securityclient/controller/RegistrationController.java
+++ security-client/src/main/java/com/diegoczajka/securityclient/controller/RegistrationController.java
@@ -72,6 +72,19 @@ public class RegistrationController {
         }
     }
 
+    @PostMapping("/changePassword")
+    public String changePassword(@RequestBody PasswordModel passwordModel){
+        User user = userService.findUserByEmail(passwordModel.getEmail());
+        if(!userService.checkIfValidOldPassword(user,passwordModel.getOldPassword())){
+            return "Invalid Old Password";
+        }
+
+        userService.changePassword(user,passwordModel.getNewPassword());
+        return "Password Changed Successfully!";
+
+
+    }
+
     @GetMapping("/resendVerifyToken")
     public String resendVerificationToken(
             @RequestParam("token") String oldToken, HttpServletRequest request) {
