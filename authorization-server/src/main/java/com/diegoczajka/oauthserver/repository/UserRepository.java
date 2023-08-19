0fd94a58ac6b97e465175620e5943c6f53ba9eed
Diego Czajka <diego.czajka@gmail.com>
2023-08-19 11:58:23 -0300

    :sparkles: feat Add load user by email

---
+++ authorization-server/src/main/java/com/diegoczajka/oauthserver/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.diegoczajka.oauthserver.repository;
+
+import com.diegoczajka.oauthserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
---
+++ authorization-server/src/main/java/com/diegoczajka/oauthserver/service/CustomUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.diegoczajka.oauthserver.service;
+
+import com.diegoczajka.oauthserver.entity.User;
+import com.diegoczajka.oauthserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("No User found!");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                true, true, true,
+                getAuthorities(List.of(user.getRole()))
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(List<String> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+        return authorities;
+    }
+}
